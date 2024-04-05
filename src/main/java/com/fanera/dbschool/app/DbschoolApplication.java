9446759f746e4cc3cc9adf5079adb191824425ed
Korshunov Michael <kolin4325@gmail.com>
2024-04-06 00:09:52 +0300

    Создание проекта

---
+++ src/main/java/com/fanera/dbschool/app/DbschoolApplication.java
@@ -0,0 +1,13 @@
+package com.fanera.dbschool.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbschoolApplication.class, args);
+    }
+
+}
